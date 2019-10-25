91ed131bc1782d724cdaf4acca153ea15fdbd289
KittyChen <kimikamis@gmail.com>
2019-10-26 00:43:57 +0800

    model relation and setting

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,13 @@
 class Food < ApplicationRecord
+  has_many :meal_foods
+  has_many :meals, through: :meal_foods
+
+  def other_foods
+    foods = meals.map { |m| m.foods }.flatten.sort
+    foods.select { |food| food.id != self.id }
+  end
+
+  def max_meals_num
+    meals.count
+  end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,12 @@
 class Meal < ApplicationRecord
+  has_many :meal_foods
+  has_many :foods, through: :meal_foods
+
+  def self.no_food_meals
+    select { |meal| !meal.has_food? }
+  end
+
+  def has_food?
+    foods.any?
+  end
 end
